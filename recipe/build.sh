92f3594242456adabea479dc1422b008c21ffffc
Moritz Pflanzer <moritz@chickadee-engineering.com>
2024-05-20 22:09:41 +0200

    Apdapt recipe for octave 9

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,4 +2,4 @@
 
 cmake -B build -D XEUS_OCTAVE_DISABLE_ARCH_NATIVE=ON ${CMAKE_ARGS}
 cmake --build build --parallel ${CPU_COUNT}
-cmake --install build --prefix ${PREFIX} 
+cmake --install build --prefix ${PREFIX}
